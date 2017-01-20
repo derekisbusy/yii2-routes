383fef21ef018db8c0d54b1f1f889da61e94b72e
Derek Gifford <derekisbusy@gmail.com>
2017-01-20 15:54:24 -0500

    Add views for route tree controller.

---
+++ views/route/index.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+use derekisbusy\rbacroutes\models\Route;
+use kartik\tree\TreeView;
+use kartik\tree\Module;
+use yii\web\View;
+
+
+$this->title = Yii::t('rbac', 'Routes');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="route-index">
+
+    <h1><?=Html::encode($this->title) ?></h1>
+
+    <?php
+    echo TreeView::widget([
+        'query' => Route::find()->addOrderBy('root, lft'),
+        'headingOptions' => ['label' => 'Route'],
+        'rootOptions' => ['label' => '<span class="text-primary">Root</span>'],
+        'fontAwesome' => false,
+        'isAdmin' => true, // @TODO : put your isAdmin getter here
+        'displayValue' => 0,
+        'cacheSettings' => ['enableCache' => true],
+        'nodeAddlViews' => [
+            Module::VIEW_PART_2 => '@vendor/derekisbusy/yii2-rbac-routes/views/route/_form'
+        ]
+    ]);
+    ?>
+
+</div>
---
+++ views/route/saveAsNew.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model derekisbusy\rbacroutes\models\Route */
+
+$this->title = Yii::t('rbac', 'Save As New {modelClass}: ', [
+    'modelClass' => 'Route',
+]). ' ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('rbac', 'Routes'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = Yii::t('rbac', 'Save As New');
+?>
+<div class="route-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+    'model' => $model,
+    ]) ?>
+
+</div>
