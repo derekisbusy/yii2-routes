f146da7a97d206a39be1fbce556a0d28d69a6731
Derek Gifford <derekisbusy@gmail.com>
2017-01-21 17:14:10 -0500

    Add _assigned template

---
+++ views/route/_assigned.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $node derekisbusy\rbacroutes\models\Route */
+
+?>
+
+<div class="route-form">
+
+form
+</div>
