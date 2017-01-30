e747409da31c5f446882dac195b7893cc4dd1829
Derek Gifford <derekisbusy@gmail.com>
2017-01-29 23:37:59 -0500

    Add status to _assigned form

--- views/route/_assigned.php
+++ views/route/_assigned.php
@@ -1,14 +1,34 @@
 <?php
 
+use kartik\widgets\SwitchInput;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $node derekisbusy\rbacroutes\models\Route */
+/* @var $node derekisbusy\routes\models\Route */
 
 ?>
 
 <div class="route-form">
+    <div class="row">
+        <div class="col-sm-8">
+<?= $form->field($node, 'status')->widget(SwitchInput::classname(), [
+    'type' => SwitchInput::CHECKBOX,
+//    'type' => SwitchInput::RADIO,
+    'items' => [
+        ['label' => 'Public', 'value' => 0],
+        ['label' => 'Protectedw', 'value' => 1],
+    ],
+    'pluginOptions' => [
+        'handleWidth'=>120,
+        'size' => 'large',
+        'onColor' => 'success',
+        'offColor' => 'danger',
+        'offText' => 'Public',
+        'onText' => 'Protected'
+    ]
 
-form
+]);?>
+        </div>
+    </div>
 </div>
