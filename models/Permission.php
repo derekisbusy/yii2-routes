2face6f96a29fc02f272366ff73b7e6390a40a6c
Derek Gifford <derekisbusy@gmail.com>
2017-01-30 22:45:52 -0500

    Add Permission model

---
+++ models/Permission.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace derekisbusy\routes\models;
+
+use dektrium\rbac\models\Permission as BasePermission;
+
+use yii\helpers\ArrayHelper;
+
+class Permission extends BasePermission
+{
+    public $id;
+    
+    public $route_ids = [];
+    
+    
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        return [
+            'create' => ['name', 'description', 'children', 'rule', 'data', 'route_ids'],
+            'update' => ['name', 'description', 'children', 'rule', 'data', 'route_ids'],
+        ];
+    }
+
+    
+//    public function getRoutes()
+//    {
+//        return $this->hasMany(Route::className(), ['id' => 'route_id'])
+//            ->viaTable(self::tableName(), ['item_id' => 'id']);
+//    }
+    
+    public function afterSave($insert, $changedAttributes)
+    {
+//        parent::afterSave($insert, $changedAttributes);
+        if (!$insert) {
+            \Yii::$app
+                ->db
+                ->createCommand()
+                ->delete(base\RouteItem::tableName(), ['item_name' => $this->name])
+                ->execute();
+        }
+        $route_ids = explode(",", $this->route_ids[0]);
+        foreach ($route_ids as $route_id) {
+            $link = new base\RouteItem;
+            $link->item_name = $this->name;
+            $link->route_id = $route_id;
+            if (!$link->save()){
+                throw new Exception("Couldn't save related data.");
+            }
+        }
+    }
+    
+    public function getRouteIds()
+    {
+        $ids = base\RouteItem::find(['item_id' => $this->id])->asArray()->all();
+        return ArrayHelper::getColumn($ids ,'route_id');
+    }
+}
