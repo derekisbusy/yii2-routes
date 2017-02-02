a8d7ab38f0ecbbfcd2caae125dd4c615b260d188
Derek Gifford <derekisbusy@gmail.com>
2017-02-02 02:23:27 -0500

    Add AuthItem model.

---
+++ models/AuthItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace derekisbusy\routes\models;
+
+use \derekisbusy\routes\models\base\AuthItem as BaseAuthItem;
+
+/**
+ * This is the model class for table "auth_item".
+ */
+class AuthItem extends BaseAuthItem
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return array_replace_recursive(parent::rules(),
+	    [
+            [['name', 'type'], 'required'],
+            [['type', 'created_at', 'updated_at'], 'integer'],
+            [['description', 'data'], 'string'],
+            [['name'], 'string', 'max' => 60],
+            [['rule_name'], 'string', 'max' => 64]
+        ]);
+    }
+	
+}
