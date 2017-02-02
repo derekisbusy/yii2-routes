b92ecd8b3d4ce155830a1971776fbe546f7421a7
Derek Gifford <derekisbusy@gmail.com>
2017-02-01 19:50:59 -0500

    Add alternate refresh button to toolbar. Minor updates.

--- views/route/index.php
+++ views/route/index.php
@@ -5,12 +5,17 @@ use derekisbusy\routes\models\Route;
 use kartik\tree\Module;
 use kartik\tree\TreeView;
 use kartik\widgets\SwitchInput;
+use yii\bootstrap\Modal;
 use yii\helpers\Url;
+use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
 
+\derekisbusy\routes\assets\RouteAsset::register($this);
 
 $this->title = Yii::t('rbac', 'Routes');
 $this->params['breadcrumbs'][] = $this->title;
+
+$this->render("_growl");
 ?>
 <?php $this->beginContent('@dektrium/rbac/views/layout.php') ?>
 
@@ -24,6 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'rootOptions' => ['label' => '<span class="text-primary">Apps</span>'],
         'fontAwesome' => false,
         'isAdmin' => false, // @TODO : put your isAdmin getter here
+        'softDelete' => false, 
         'displayValue' => 0,
         'cacheSettings' => ['enableCache' => true],
         'nodeActions' => [
@@ -37,39 +43,49 @@ $this->params['breadcrumbs'][] = $this->title;
             Module::VIEW_PART_2 => '@vendor/derekisbusy/yii2-routes/views/route/_assigned'
         ],
         'toolbar' => [
+//            'add' => [
+//                'icon' => 'plus',
+//                'options' => ['title' => Yii::t('routes', 'Add route'), 'data-toggle' => "modal", 'data-target' => "#addRouteModal" , 'id' => 'btnAddRoute']
+//            ],
+            'update' => [
+                'icon' => 'refresh',
+                'options' => ['title' => Yii::t('routes', 'Refresh'), 'id' => 'btnRefreshRoutes' ],
+                'url' => Url::to(['refresh'])
+                
+            ],
             TreeView::BTN_CREATE => [
                 'icon' => 'plus',
-                'options' => ['title' => Yii::t('kvtree', 'Add new'), 'disabled' => true]
+                'options' => ['title' => Yii::t('routes', 'Add new'), 'disabled' => true]
             ],
             TreeView::BTN_CREATE_ROOT => [
                 'icon' => false ? 'tree' : 'tree-conifer',
-                'options' => ['title' => Yii::t('kvtree', 'Add new root')]
+                'options' => ['title' => Yii::t('routes', 'Add new root')]
             ],
             TreeView::BTN_REMOVE => [
                 'icon' => 'trash',
-                'options' => ['title' => Yii::t('kvtree', 'Delete'), 'disabled' => true]
+                'options' => ['title' => Yii::t('routes', 'Delete'), 'disabled' => true]
             ],
             TreeView::BTN_SEPARATOR,
             TreeView::BTN_MOVE_UP => [
                 'icon' => 'arrow-up',
-                'options' => ['title' => Yii::t('kvtree', 'Move Up'), 'disabled' => true, 'style' => "display: none"]
+                'options' => ['title' => Yii::t('routes', 'Move Up'), 'disabled' => true, 'style' => "display: none"]
             ],
             TreeView::BTN_MOVE_DOWN => [
                 'icon' => 'arrow-down',
-                'options' => ['title' => Yii::t('kvtree', 'Move Down'), 'disabled' => true, 'style' => "display: none"]
+                'options' => ['title' => Yii::t('routes', 'Move Down'), 'disabled' => true, 'style' => "display: none"]
             ],
             TreeView::BTN_MOVE_LEFT => [
                 'icon' => 'arrow-left',
-                'options' => ['title' => Yii::t('kvtree', 'Move Left'), 'disabled' => true, 'style' => "display: none"]
+                'options' => ['title' => Yii::t('routes', 'Move Left'), 'disabled' => true, 'style' => "display: none"]
             ],
             TreeView::BTN_MOVE_RIGHT => [
                 'icon' => 'arrow-right',
-                'options' => ['title' => Yii::t('kvtree', 'Move Right'), 'disabled' => true, 'style' => "display: none"]
+                'options' => ['title' => Yii::t('routes', 'Move Right'), 'disabled' => true, 'style' => "display: none"]
             ],
             TreeView::BTN_SEPARATOR,
             TreeView::BTN_REFRESH => [
                 'icon' => 'refresh',
-                'options' => ['title' => Yii::t('kvtree', 'Refresh')],
+                'options' => ['title' => Yii::t('routes', 'Refresh'), 'style' => "display: none"],
                 'url' => Yii::$app->request->url
             ],
         ]
@@ -83,5 +99,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ]); ?>
 </div>
 <?php Pjax::end() ?>
+
+
 
 <?php $this->endContent() ?>
