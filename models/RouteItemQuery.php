dafece7994ffb7bf0ae830baf367d31aa2089c78
Derek Gifford <derekisbusy@gmail.com>
2019-06-27 14:41:41 -0400

    Add class

---
+++ models/RouteItemQuery.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace derekisbusy\routes\models;
+
+/**
+ * This is the ActiveQuery class for [[RouteItem]].
+ *
+ * @see Route
+ */
+class RouteItemQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        $this->andWhere('[[status]]=1');
+        return $this;
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return Route[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return Route|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
